501fc3cf9616ed26f409ce18aeaf5b461e7cf807
Hannes Achleitner <hannes.software@gmx.at>
2026-08-29 09:09:40 +0200

    Apply necessary changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -146,7 +146,7 @@ dependencies {
 
     // Firebase
     implementation(platform("com.google.firebase:firebase-bom:34.18.0"))
-    implementation("com.google.firebase:firebase-firestore-ktx")
-    implementation("com.google.firebase:firebase-auth-ktx")
-    implementation("com.google.firebase:firebase-storage-ktx")
+    implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-storage")
 }
