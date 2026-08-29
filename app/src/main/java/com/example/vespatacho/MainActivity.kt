e0edf0c0f197038f73e8c76485af270f99ad3d40
Hannes Achleitner <hannes.software@gmx.at>
2026-08-29 09:40:06 +0200

    Github app update

--- app/src/main/java/com/example/vespatacho/MainActivity.kt
+++ app/src/main/java/com/example/vespatacho/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.example.vespatacho
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import com.example.vespatacho.ui.HomeScreen
+import info.hannes.github.AppUpdateHelper
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent { HomeScreen() }
+
+        AppUpdateHelper.checkForNewVersion(this, BuildConfig.GIT_REPOSITORY)
     }
 }
